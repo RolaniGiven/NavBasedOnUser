396cc23d492f3c80b802216cf82ad5655e9b7015
RolaniGiven <Rolanimaluleka00@gmail.com>
2023-10-24 15:15:18 +0200

    All pages are working,navbar and statsbar aswell

--- NavBar/src/app/app-routing.module.ts
+++ NavBar/src/app/app-routing.module.ts
@@ -1,7 +1,15 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { DashboardComponent } from './pages/dashboard/dashboard.component';
+import { NominateComponent } from './pages/nominate/nominate.component';
+import { TrailblazerComponent } from './pages/trailblazer/trailblazer.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+   {path: "dashboard", component: DashboardComponent  },   
+   { path: 'nominate', component: NominateComponent },
+   { path: 'trailblaizer', component: TrailblazerComponent }
+
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
